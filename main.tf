de883f3c5283f3629ca0b271a023dca2a105ffac
Jeremiah Norris <jjno91@gmail.com>
2019-01-17 16:01:58 -0600

    add inline s3 access policy for elb log delivery

--- main.tf
+++ main.tf
@@ -21,11 +21,34 @@ resource "aws_instance" "this" {
 # S3
 #################################################
 
+data "aws_elb_service_account" "this" {}
+
 resource "aws_s3_bucket" "this" {
   bucket        = "${var.env}-logs"
   acl           = "log-delivery-write"
   force_destroy = "true"
   tags          = "${var.tags}"
+
+  policy = <<POLICY
+{
+  "Id": "Policy",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.env}-logs/*",
+      "Principal": {
+        "AWS": [
+          "${data.aws_elb_service_account.this.arn}"
+        ]
+      }
+    }
+  ]
+}
+POLICY
 
   server_side_encryption_configuration {
     rule {
